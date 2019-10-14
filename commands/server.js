fc24c6a3e83cc6564745535598cd4cf21758a576
Bharat Kathi <bharat1031@gmail.com>
2019-10-14 16:13:46 -0700

    Updated server ip

--- commands/server.js
+++ commands/server.js
@@ -5,13 +5,13 @@ module.exports = {
 	name: 'server',
 	description: 'Get NovaSquad SMP server status',
 	execute(message, args) {
-        request('https://api.mcsrvstat.us/2/mc.samstep.ga:25566', {json:true}, (err, res, body) => {
+        request('https://api.mcsrvstat.us/2/35.227.157.239', {json:true}, (err, res, body) => {
             if (body.online) {
                 // Server is online
                 console.log('Server is online!');
                 message.channel.send(new Discord.RichEmbed()
-                    .setTitle('NovaSquad SMP')
-                    .setDescription('mc.samstep.ga:25566')
+                    .setTitle('Airpods SMP')
+                    .setDescription('35.227.157.239')
                     .setColor('#42f477')
                     .addField('Version', body.version)
                     .addField('Online', `${body.players.online}/${body.players.max}`)
@@ -24,7 +24,7 @@ module.exports = {
                 console.log('Server not online!');
                 message.channel.send(new Discord.RichEmbed()
                 .setTitle('NovaSquad SMP')
-                    .setDescription('mc.samstep.ga:25566')
+                    .setDescription('35.227.157.239')
                     .setColor('#f44242')
                     .addField('Offline', `--------`)
                     .setTimestamp()
