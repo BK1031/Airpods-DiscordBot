88c968222b9798e4ca276c8d19b701717702de0f
Bharat Kathi <bharat1031@gmail.com>
2019-10-17 18:00:14 -0700

    Added maintenance eta support

--- commands/maintenance.js
+++ commands/maintenance.js
@@ -9,5 +9,13 @@ module.exports = {
         botconfig.maintenance = !botconfig.maintenance;
         console.log("Updated maintenance mode: " + botconfig.maintenance);
         message.channel.send("Updated maintenance mode: " + botconfig.maintenance);
+        if (args.length == 0) {
+            botconfig.maintenance_end = "in a bit"
+            message.channel.send('Set default maintenance eta: ' + botconfig.maintenance_end);
+        }
+        else {
+            botconfig.maintenance_end = args.join(" ");
+            message.channel.send('Set maintenance eta: ' + botconfig.maintenance_end);
+        }
 	},
 };
--- commands/server.js
+++ commands/server.js
@@ -27,7 +27,7 @@ module.exports = {
                         .setTitle('Airpods SMP')
                         .setDescription('35.227.157.239')
                         .setColor('#ebde34')
-                        .addField('Scheduled Maintenance', `Server will be back 10/17/19 2:30 PM`)
+                        .addField('Scheduled Maintenance', `Server will be back ${botconfig.maintenance_end}`)
                         .setTimestamp()
                     );
                 }
